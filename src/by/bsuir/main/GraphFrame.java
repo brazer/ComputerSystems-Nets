00acaee8a169f3b864d05ba09b44e1bfe6f61940
brazer <salanevich.anatol@gmail.com>
2014-04-28 00:38:25 +0300

    Build of tree

--- src/by/bsuir/main/GraphFrame.java
+++ src/by/bsuir/main/GraphFrame.java
@@ -2,6 +2,7 @@ package by.bsuir.main;
 
 import java.awt.Container;
 import java.util.ArrayList;
+import java.util.Stack;
 
 import javax.swing.JFrame;
 
@@ -22,6 +23,8 @@ public class GraphFrame extends JFrame{
 	
 	public GraphFrame(char[] charArray) {
 		function = charArray;
+		ParsingFunction pf = new ParsingFunction();
+		Tree tree = pf.parseFunction(function);
         Container content = getContentPane();
         graph = new Graph();
         content.add(graph);
@@ -36,31 +39,93 @@ public class GraphFrame extends JFrame{
 
 class ParsingFunction {
 	
-	public static void parseFunction(char[] str) {
-		buildTree(str);
+	private Stack<Tree> treeBuilding = new Stack<Tree>();
+	private boolean highFlag = false, lowFlag = false;
+	
+	public Tree parseFunction(char[] str) {
+		return buildTree(str);
 	}
 	
-	private static void buildTree(char[] str) {
+	private Tree buildTree(char[] str) {
 		for (int i=0; i<str.length; i++) {
 			if (str[i]=='x') {
-				
+				char[] number = {str[i+1], str[i+2]};
+				Leaf l = new Leaf(getNumber(number));
+				if (highFlag) {
+					Node n = (Node) treeBuilding.pop();
+					Tree t = treeBuilding.pop();
+					n.setLeft(l);
+					n.setRight(t);
+					treeBuilding.push(n);
+					highFlag = false;
+				} else treeBuilding.push(l);
 			}
-			if (str[i]=='*') {
-				
+			if (str[i]=='*') processHigh(str[i]);
+			if (str[i]=='/') processHigh(str[i]);
+			if (str[i]=='+') processLow(str[i]);
+			if (str[i]=='-') processLow(str[i]);
+			if (str[i]=='F' | str[i]=='f') {
+				Node f = new Node(str[i]);
+				i += 2;
+				char arg[] = getArgument(str, i);
+				ParsingFunction p = new ParsingFunction();
+				Tree t = p.buildTree(arg);
+				f.setLeft(t);
+				treeBuilding.push(f);
+				i += arg.length + 1;
 			}
-			if (str[i]=='/') {
-				
-			}
-			if (str[i]=='+') {
-				
-			}
-			if (str[i]=='-') {
-				
-			}
-			if (str[i]=='f') {
-				
-			}
+		}		
+		while(treeBuilding.size()>1) processStack();		
+		return treeBuilding.pop();
+	}
+	
+	private static int getNumber(char[] s) {
+		int n = -1;
+		try {
+			if (s[1]=='0')
+				n = Integer.parseInt(String.valueOf(s));
+			else n = Integer.parseInt(String.valueOf(s[0]));
+		} catch(NumberFormatException ex) {
+			ex.printStackTrace(); 
 		}
+		return n;			
+	}
+	
+	private void processHigh(char ch) {
+		highFlag = true;
+		Node node = new Node(ch);
+		treeBuilding.push(node);
+	}
+	
+	private void processLow(char ch) {
+		Node n = new Node(ch);
+		checkLowFlag();
+		treeBuilding.push(n);
+	}
+	
+	private void checkLowFlag() {
+		if (lowFlag) processStack();
+		else lowFlag = true;
+	}
+	
+	private void processStack() {
+		Tree tRight = treeBuilding.pop();
+		Node tOper = (Node) treeBuilding.pop();
+		Tree tLeft = treeBuilding.pop();
+		tOper.setLeft(tLeft);
+		tOper.setRight(tRight);
+		treeBuilding.push(tOper);
+	}
+	
+	private char[] getArgument(char[] expr, int i) {		
+		int len = 0, beg = i;
+		while(expr[i]!=')') {
+			len++; i++;
+		}
+		char ch[] = new char[len];		
+		for (int j=0, k=beg; j<len; k++, j++) 
+			ch[j] = expr[k];
+		return ch;
 	}
 	
 }
--- src/by/bsuir/main/Tree.java
+++ src/by/bsuir/main/Tree.java
@@ -21,25 +21,36 @@ public abstract class Tree {
 
 class Node extends Tree {
 	
-	private Operation OPER;
-	private Leaf left, right;
+	private char OPER;
+	private Tree left=null, right=null;
 	
-	public Node(Operation oper, Leaf l, Leaf r) {
+	public Node(char oper) {
 		this.OPER = oper;
-		left = l;
-		right = r;
 	}
 	
-	public Operation getOperation() {
+	public char getOperation() {
 		return OPER;
 	}
 	
-	public Leaf getLeft() {
+	public Tree getLeft() {
 		return left;
 	}
 	
-	public Leaf getRight() {
+	public void setLeft(Tree l) {
+		left = l;
+	}
+	
+	public Tree getRight() {
 		return right;
+	}
+	
+	public void setRight(Tree r) {
+		right = r;
+	}
+	
+	public boolean isReady() {
+		if (left!=null & right!=null) return true;
+		else return false;
 	}
 	
 	class Operation {
@@ -50,19 +61,38 @@ class Node extends Tree {
 
 class Leaf extends Tree {
 	
-	private Variable X;
+	private int X;
 	
-	public Leaf(Variable v) {
+	public Leaf(int v) {
 		X = v;
 	}
 	
-	public Variable getX() {
+	public int getX() {
 		return X;
 	}
 	
-	class Variable {
-		public static final int 
-			X1=0, X2=1, X3=2, X4=3, X5=4, X6=5, X7=6, X8=7, X9=8, X10=9;
+	static class Variable {
+		
+		private static Exception ex = new Exception("Ошибка разбора переменной X");
+		public static final String 
+			X1="x1", X2="x2", X3="x3", X4="x4", X5="x5", 
+			X6="x6", X7="x7", X8="x8", X9="x9", X10="x10";
+		
+		public static String getVariable(int n) throws Exception {
+			switch(n) {
+			case 1: return X1;
+			case 2: return X2;
+			case 3: return X3;
+			case 4: return X4;
+			case 5: return X5;
+			case 6: return X6;
+			case 7: return X7;
+			case 8: return X8;
+			case 9: return X9;
+			case 10: return X10;
+			}
+			throw ex;
+		}
 	}
 	
 }
